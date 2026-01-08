cf784284590d58e8408ac9198a1e63559c3a3aca
ajaybkg777-crypto <ajaybkg777@gmail.com>
2026-01-08 16:51:10 +0530

    Update device.js

--- src/utils/device.js
+++ src/utils/device.js
@@ -1,16 +1,8 @@
 export const getDeviceId = () => {
-  if (typeof window === "undefined") return null;
-
   let id = localStorage.getItem("device_id");
-
   if (!id) {
-    if (window.crypto && crypto.randomUUID) {
-      id = crypto.randomUUID();
-    } else {
-      id = "dev-" + Math.random().toString(36).slice(2, 11);
-    }
+    id = crypto.randomUUID ? crypto.randomUUID() : Math.random().toString(36).substr(2, 9);
     localStorage.setItem("device_id", id);
   }
-
   return id;
 };
